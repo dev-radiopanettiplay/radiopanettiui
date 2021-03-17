070b2236a9f60a7b207720d1626589a2ad40270c
Andrea Ottomanelli <ottoandry@hotmail.it>
2021-03-17 20:07:14 +0100

    Update components.js

--- dist/js/components.js
+++ dist/js/components.js
@@ -25,7 +25,7 @@ class rpuigallery extends HTMLElement {
 		  }
 		}
 window.customElements.define('rpui-gallery', rpuigallery);
-class rpuigallery extends HTMLElement {
+class rpuiplayer extends HTMLElement {
 		  connectedCallback() {
 		    var immagine=this.getAttribute("immagine") || "", mp4=this.getAttribute("mp4") || "";
 			this.innerHTML = `<video id="my-video" class="video-js" controls preload="auto" width="640" height="264" poster="`+immagine+`" data-setup="{}"><source src="`+mp4+`" type="video/mp4" /></video><script src="https://vjs.zencdn.net/7.10.2/video.min.js"></script>`;
